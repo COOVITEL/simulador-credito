7272a740366a0ce6ea5204149201ed48713f57f9
ManuRodriguezC <estebanclimb@gmail.com>
2025-02-19 11:51:14 -0500

    Update controls

--- src/utils/searchTasaSocial.ts
+++ src/utils/searchTasaSocial.ts
@@ -6,15 +6,15 @@ export function searchTasaSocial(list: Sociales, cuota: number) {
     // Dependiendo del numero de cuotas del credito a solicitar
     if (cuota > 1 && cuota < 13) {
         tasa = list.tasa6
-    } else if (cuota >= 13 && cuota < 24) {
+    } else if (cuota >= 13 && cuota <= 24) {
         tasa = list.tasa12
-    } else if (cuota >= 25 && cuota < 36) {
+    } else if (cuota >= 25 && cuota <= 36) {
         tasa = list.tasa24
-    } else if (cuota >= 37 && cuota < 48) {
+    } else if (cuota >= 37 && cuota <= 48) {
         tasa = list.tasa36
-    } else if (cuota >= 49 && cuota < 60) {
+    } else if (cuota >= 49 && cuota <= 60) {
         tasa = list.tasa48
-    } else if (cuota >= 61 && cuota < 72) {
+    } else if (cuota >= 61 && cuota <=   72) {
         tasa = list.tasa60
     } else if (cuota >= 73 && cuota <= 84) {
         tasa = list.tasa72
