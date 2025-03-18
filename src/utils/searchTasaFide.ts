eb33a403619805f99e7297b780e5e13e85288c2d
ManuRodriguezC <estebanclimb@gmail.com>
2025-03-18 16:19:52 -0500

    Check and fix functions search tasa in fidelizacion and social

--- src/utils/searchTasaFide.ts
+++ src/utils/searchTasaFide.ts
@@ -3,19 +3,19 @@ import { Fidelizacion } from "../store/types";
 export function searchTasaFide(list: Fidelizacion, cuota: number) {
     // Esta funcion busca la tasa de la linea de fidelizacion, dependiendo del numero de cuotas del credito
     let tasa = 0
-    if (cuota > 0 && cuota < 9) {
+    if (cuota > 0 && cuota <= 6) {
         tasa = list.tasa6
-    } else if (cuota >= 9 && cuota < 19) {
+    } else if (cuota > 6 && cuota <= 12) {
         tasa = list.tasa12
-    } else if (cuota >= 19 && cuota < 30) {
+    } else if (cuota > 12 && cuota <= 24) {
         tasa = list.tasa24
-    } else if (cuota >= 30 && cuota < 42) {
+    } else if (cuota > 24 && cuota <= 36) {
         tasa = list.tasa36
-    } else if (cuota >= 42 && cuota < 54) {
+    } else if (cuota > 36 && cuota <= 48) {
         tasa = list.tasa48
-    } else if (cuota >= 54 && cuota < 66) {
+    } else if (cuota > 48 && cuota <= 60) {
         tasa = list.tasa60
-    } else if (cuota >= 66) {
+    } else if (cuota > 60 && cuota < 72) {
         tasa = list.tasa72
     } else {
         tasa = 0
--- src/utils/searchTasaSocial.ts
+++ src/utils/searchTasaSocial.ts
@@ -4,22 +4,22 @@ export function searchTasaSocial(list: Sociales, cuota: number) {
     // Esta funcion busca el valor de la tasa para las lineas no sociales
     let tasa = 0
     // Dependiendo del numero de cuotas del credito a solicitar
-    if (cuota > 1 && cuota < 13) {
+    if (cuota > 0 && cuota <= 6) {
         tasa = list.tasa6
-    } else if (cuota >= 13 && cuota <= 24) {
+    } else if (cuota > 6 && cuota <= 12) {
         tasa = list.tasa12
-    } else if (cuota >= 25 && cuota <= 36) {
+    } else if (cuota > 12 && cuota <= 24) {
         tasa = list.tasa24
-    } else if (cuota >= 37 && cuota <= 48) {
+    } else if (cuota > 24 && cuota <= 36) {
         tasa = list.tasa36
-    } else if (cuota >= 49 && cuota <= 60) {
+    } else if (cuota > 36 && cuota <= 48) {
         tasa = list.tasa48
-    } else if (cuota >= 61 && cuota <=   72) {
+    } else if (cuota > 48 && cuota <= 60) {
         tasa = list.tasa60
-    } else if (cuota >= 73 && cuota <= 84) {
+    } else if (cuota > 60 && cuota <= 72) {
         tasa = list.tasa72
-    } else {
-        tasa = 0
+    } else if (cuota > 72 && cuota <= 84) {
+        tasa = list.tasa84
     }
     return tasa
 }
